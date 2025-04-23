106557cff87dce726d260bcc87ee2441b402b1ae
AmyPap <amypapakosta@gmail.com>
2025-04-23 12:20:38 +0200

    Lade till ändringar i controllers

--- DigitalStore/src/main/java/com/example/DigitalStore/controller/SizesController.java
+++ DigitalStore/src/main/java/com/example/DigitalStore/controller/SizesController.java
@@ -5,7 +5,7 @@ import com.example.DigitalStore.repository.SizesRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
+
 import java.util.Map;
 import java.util.stream.Collectors;
 
